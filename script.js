8a42828080fb61b2b5dfc2bd033c851489b128a1
Rafael Guzman <rega1237@gmail.com>
2022-04-05 13:06:43 -0400

    fixed linters errors

--- script.js
+++ script.js
@@ -3,33 +3,33 @@ const menuBtn = document.querySelector('.nav-bar-mobile');
 let menuOpen = false;
 
 menuBtn.addEventListener('click', () => {
-    if (mobileElement.style.display === "block") {
-        mobileElement.style.display = "none";
-    } else {
-        mobileElement.style.display = "block";
+  if (mobileElement.style.display === 'block') {
+    mobileElement.style.display = 'none';
+  } else {
+    mobileElement.style.display = 'block';
     }
 });
 
 menuBtn.addEventListener('click', () => {
-    if(!menuOpen) {
-        menuBtn.classList.add('open');
-        menuBtn.style.marginRight = '25px';
-        menuOpen = true;
-    } else {
-        menuBtn.classList.remove('open');
-        menuBtn.style.marginRight = '36px';
-        menuOpen = false; 
-    }
+  if (!menuOpen) {
+    menuBtn.classList.add('open');
+    menuBtn.style.marginRight = '25px';
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuBtn.style.marginRight = '36px';
+    menuOpen = false;
+  }
 });
 
 mobileElement.addEventListener('click', () => {
-    if(mobileElement.style.display === "block"){
-        mobileElement.style.display = "none";
-        menuBtn.classList.remove('open');
-        menuOpen = false;
-    } else {
-        mobileElement.style.display = "block";
-        menuBtn.classList.add('open');
-        menuOpen = true;
-    }
+  if (mobileElement.style.display === 'block') {
+    mobileElement.style.display = 'none';
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  } else {
+    mobileElement.style.display = 'block';
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  }
 });
