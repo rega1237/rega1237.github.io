8926a6b0e806ef65b020d5044becf5109bf7f669
Rafael Guzman <rega1237@gmail.com>
2022-04-05 12:24:26 -0400

    Add style mobile-links, add button transform to 'x' with css and javascript event

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const mobileElement = document.getElementById('mobile-links');
 const menuBtn = document.querySelector('.nav-bar-mobile');
+let menuOpen = false;
+
 menuBtn.addEventListener('click', () => {
     if (mobileElement.style.display === "block") {
         mobileElement.style.display = "none";
     } else {
         mobileElement.style.display = "block";
+    }
+});
+
+menuBtn.addEventListener('click', () => {
+    if(!menuOpen) {
+        menuBtn.classList.add('open');
+        menuBtn.style.marginRight = '25px';
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuBtn.style.marginRight = '36px';
+        menuOpen = false; 
     }
 });
