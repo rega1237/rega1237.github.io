872099b52eb095b932110e73eab7776606aba9bf
latinogan <roger.castillo.arce@gmail.com>
2022-04-07 20:34:58 -0500

    create object cards script.js file

--- script.js
+++ script.js
@@ -32,3 +32,68 @@ mobileElement.addEventListener('click', () => {
         menuOpen = true;
     }
 });
+
+
+const cards = [
+    {
+      item: 'firts',
+      box: 'card',
+      imgHolder: 'image-card-holder',
+      cardTitleHolder: 'text-work-title',
+      textCardTitle: 'Multi-Post Stories Gain+Glory',
+      cardTagsHolder: 'card-tags',
+      technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
+      technologiesTemplate: 'tag-element',
+      button: ['see-project-btn', 'btn'],
+    },
+    {
+      box: 'card',
+      imgHolder: 'image-card-holder',
+      cardTitleHolder: 'text-work-title',
+      textCardTitle: 'Multi-Post Stories Gain+Glory',
+      cardTagsHolder: 'card-tags',
+      technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
+      technologiesTemplate: 'tag-element',
+      button: ['see-project-btn', 'btn'],
+    },
+    {
+      box: 'card',
+      imgHolder: 'image-card-holder',
+      cardTitleHolder: 'text-work-title',
+      textCardTitle: 'Multi-Post Stories Gain+Glory',
+      cardTagsHolder: 'card-tags',
+      technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
+      technologiesTemplate: 'tag-element',
+      button: ['see-project-btn', 'btn'],
+    },
+    {
+      box: 'card',
+      imgHolder: 'image-card-holder',
+      cardTitleHolder: 'text-work-title',
+      textCardTitle: 'Multi-Post Stories Gain+Glory',
+      cardTagsHolder: 'card-tags',
+      technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
+      technologiesTemplate: 'tag-element',
+      button: ['see-project-btn', 'btn'],
+    },
+    {
+      box: 'card',
+      imgHolder: 'image-card-holder',
+      cardTitleHolder: 'text-work-title',
+      textCardTitle: 'Multi-Post Stories Gain+Glory',
+      cardTagsHolder: 'card-tags',
+      technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
+      technologiesTemplate: 'tag-element',
+      button: ['see-project-btn', 'btn'],
+    },
+    {
+      box: 'card',
+      imgHolder: 'image-card-holder',
+      cardTitleHolder: 'text-work-title',
+      textCardTitle: 'Multi-Post Stories Gain+Glory',
+      cardTagsHolder: 'card-tags',
+      technologies: ['Ruby on rails', 'CSS', 'JavaScript', 'HTML'],
+      technologiesTemplate: 'tag-element',
+      button: ['see-project-btn', 'btn'],
+    },
+  ];
